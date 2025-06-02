33a78a0b8c3a8e137b1d234a16a285f40500b249
rageshganeshkumar <ragesh.ganeshkumar@hashicorp.com>
2025-06-02 10:00:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,12 +37,47 @@ resource "random_string" "example5" {
   upper   = false
 }
 
+resource "random_string" "example6" {
+  length  = var.length
+  special = false
+  upper   = false
+}
+
+resource "random_string" "example7" {
+  length  = var.length
+  special = false
+  upper   = false
+}
+
+resource "random_string" "example8" {
+  length  = var.length
+  special = false
+  upper   = false
+}
+
+resource "random_string" "example9" {
+  length  = var.length
+  special = false
+  upper   = false
+}
+
+resource "random_string" "example10" {
+  length  = var.length
+  special = false
+  upper   = false
+}
+
 output "random_strings" {
   value = [
     random_string.example1.result,
     random_string.example2.result,
     random_string.example3.result,
     random_string.example4.result,
-    random_string.example5.result
+    random_string.example5.result,
+    random_string.example6.result,
+    random_string.example7.result,
+    random_string.example8.result,
+    random_string.example9.result,
+    random_string.example10.result
   ]
 }
